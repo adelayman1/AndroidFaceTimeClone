ad10c3405ac9c6ab16d518aadff1e30686679832
adelayman1 <85571327+adelayman1@users.noreply.github.com>
2022-06-25 02:38:39 +0200

    Update FcmApiService.kt

--- app/src/main/java/com/adel/facetimeclone/data/api/FcmApiService.kt
+++ app/src/main/java/com/adel/facetimeclone/data/api/FcmApiService.kt
@@ -9,7 +9,7 @@ import retrofit2.http.POST
 interface FcmApiService {
     @Headers(
         "Content-Type:application/json",
-        "Authorization:key=AAAAfOUKvh4:APA91bFnBpXUB2Qr0Th6nLVFQXMdb1n5-WyeBfrnCDYEc3xeQJ30WCm7uE5GreNhRjpaIDY6-W_ChZ91ncVbD_jHnZHmHjoUltKt_rVdkt1vkcaZX4WBsLVkK_vNRyJHL9q77OIY_U3R"
+        "Authorization:key="
     )
     @POST("fcm/send")
     suspend fun sendFcmCallData(@Body invitationModel: CallInvitationModel): Response<Any>
