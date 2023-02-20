bd2c1d2ee22fb101048b922c3dcf48592ce62958
Adel Ayman <adelayman0000@gmail.com>
2023-02-20 22:30:25 +0200

    Add api extensions

---
+++ app/src/main/java/com/example/facetimeclonecompose/data/utilities/Extensions.kt
@@ -0,0 +1,22 @@
+package com.example.facetimeclonecompose.data.utilities
+
+import com.example.facetimeclonecompose.data.sources.remote.responseModels.BaseApiResponse
+import io.ktor.client.features.ClientRequestException
+import io.ktor.client.statement.readText
+import io.ktor.utils.io.charsets.Charset
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+
+suspend fun <T> makeRequestAndHandleErrors(block: suspend () -> T): T {
+    return try {
+        withContext(Dispatchers.IO) { block() }
+    } catch (e: ClientRequestException) {
+        throw e.handleError()
+    }
+}
+suspend fun ClientRequestException.handleError(): Exception {
+    val content = response.readText(Charset.defaultCharset())
+    return Exception(Json.decodeFromString<BaseApiResponse<Any>>(content).message)
+}
