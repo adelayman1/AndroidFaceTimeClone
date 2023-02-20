19e6fe72afbe32f3f2f69c2527f42a41d400dd6f
Adel Ayman <adelayman0000@gmail.com>
2023-02-20 21:49:27 +0200

    Init Constants

--- app/src/main/java/com/example/facetimeclonecompose/data/utilities/Constants.kt
+++ app/src/main/java/com/example/facetimeclonecompose/data/utilities/Constants.kt
@@ -1,5 +1,6 @@
 package com.example.facetimeclonecompose.data.utilities
 
+
 object Constants {
     const val BASE_URL:String = "http://127.0.0.1:8080"
     const val USER_TOKEN_SHARED_PREFERENCES_KEY:String = "UserToken"
