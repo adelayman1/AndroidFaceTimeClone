3502a615a38c1258fd594864fa9cf8fb01fc2134
Adel Ayman <adelayman0000@gmail.com>
2023-02-20 21:48:59 +0200

    Init Constants

---
+++ app/src/main/java/com/example/facetimeclonecompose/data/utilities/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.facetimeclonecompose.data.utilities
+
+object Constants {
+    const val BASE_URL:String = "http://127.0.0.1:8080"
+    const val USER_TOKEN_SHARED_PREFERENCES_KEY:String = "UserToken"
+    const val USER_SHARED_PREFERENCES_KEY:String = "user"
+}
